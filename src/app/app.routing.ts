56b4140e62aa215b4eff2e00f8a1011d9523b116
Sujendra shrestha <sujendra.shrestha@itonics.de>
2019-12-23 11:53:38 +0545

    Initial commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,13 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { AuthGuard } from './_helpers';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '' }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
